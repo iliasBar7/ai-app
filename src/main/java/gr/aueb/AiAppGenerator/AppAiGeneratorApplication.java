507087e705bac2857211e6f31a43a6275a03b86a
ilias Barbitsiotis <hbarbitsiotis@windowslive.com>
2025-08-11 18:47:32 +0300

    initial commit

---
+++ src/main/java/gr/aueb/AiAppGenerator/AppAiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.AiAppGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAiGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/AiAppGenerator/AppAiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.AiAppGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
